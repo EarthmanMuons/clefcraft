f2715b91d7c5a67c090e5e72bbfaceb3170ee980
Aaron Bull Schaefer <aaron@elasticdog.com>
2024-08-15 13:23:12 -0700

    refactor: move theory into theory.old in preparation for rewrite
    I want to work through what different structures for the music theory
    elements would look like, but don't want to break everything at once.
    I'll build it up again to see how things work and then eventually drop
    the old representation if it's better.

--- src/root.zig
+++ src/root.zig
@@ -1,16 +1,16 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub const Interval = @import("theory/interval.zig").Interval;
-pub const Note = @import("theory/note.zig").Note;
-pub const Pitch = @import("theory/pitch.zig").Pitch;
-pub const Scale = @import("theory/scale.zig").Scale;
+pub const Interval = @import("theory.old/interval.zig").Interval;
+pub const Note = @import("theory.old/note.zig").Note;
+pub const Pitch = @import("theory.old/pitch.zig").Pitch;
+pub const Scale = @import("theory.old/scale.zig").Scale;
 
 test {
     // Run all unit tests.
-    _ = @import("theory/interval.zig");
-    _ = @import("theory/key_signature.zig");
-    _ = @import("theory/note.zig");
-    _ = @import("theory/pitch.zig");
-    _ = @import("theory/scale.zig");
+    _ = @import("theory.old/interval.zig");
+    _ = @import("theory.old/key_signature.zig");
+    _ = @import("theory.old/note.zig");
+    _ = @import("theory.old/pitch.zig");
+    _ = @import("theory.old/scale.zig");
 }
