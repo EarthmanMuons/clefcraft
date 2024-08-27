f5dd6002f033bfbce11dc1d887465aabc27fc35e
Aaron Bull Schaefer <aaron@elasticdog.com>
2024-08-27 15:09:14 -0700

    feat: ensure naming persistence works for notes

--- src/theory/note.zig
+++ src/theory/note.zig
@@ -5,12 +5,26 @@ const c = @import("constants.zig");
 
 pub const Note = struct {
     midi: u7,
+    name: Spelling,
+
+    pub const Spelling = struct {
+        let: Letter,
+        acc: Accidental,
+    };
 
     pub const Letter = enum { c, d, e, f, g, a, b };
     pub const Accidental = enum { double_flat, flat, natural, sharp, double_sharp };
 
     pub fn init(let: Letter, acc: Accidental, oct: i8) !Note {
-        const base: i16 = baseSemitones(let);
+        const base: i16 = switch (let) {
+            .c => 0,
+            .d => 2,
+            .e => 4,
+            .f => 5,
+            .g => 7,
+            .a => 9,
+            .b => 11,
+        };
         const offset: i4 = switch (acc) {
             .double_flat => -2,
             .flat => -1,
@@ -22,56 +36,66 @@ pub const Note = struct {
         if (midi < 0 or c.midi_max < midi) {
             return error.NoteOutOfRange;
         }
-        return .{ .midi = @intCast(midi) };
+        return .{ .midi = @intCast(midi), .name = .{ .let = let, .acc = acc } };
     }
 
-    fn baseSemitones(let: Letter) u4 {
-        return switch (let) {
-            .c => 0,
-            .d => 2,
-            .e => 4,
-            .f => 5,
-            .g => 7,
-            .a => 9,
-            .b => 11,
-        };
-    }
-
-    pub fn letter(self: Note) Letter {
-        const pc = self.pitchClass();
-        return switch (pc) {
-            0, 1 => .c,
-            2, 3 => .d,
-            4 => .e,
-            5, 6 => .f,
-            7, 8 => .g,
-            9, 10 => .a,
-            11 => .b,
-            else => unreachable,
-        };
-    }
-
-    pub fn accidental(self: Note) Accidental {
-        const pc: i8 = self.pitchClass();
-        const let = self.letter();
-        const base = baseSemitones(let);
-        const diff = @mod(pc - base + c.semis_per_oct, c.semis_per_oct);
-        return switch (diff) {
-            10 => .double_flat,
-            11 => .flat,
-            0 => .natural,
-            1 => .sharp,
-            2 => .double_sharp,
-            else => unreachable,
-        };
+    pub fn fromMidi(midi: u7) Note {
+        return .{ .midi = midi, .name = spellWithSharps(midi) };
     }
 
     pub fn octave(self: Note) i8 {
-        return @divFloor(@as(i8, self.midi), c.semis_per_oct) - 1;
+        const oct = @divFloor(@as(i8, self.midi), c.semis_per_oct) - 1;
+
+        // Handle octave boundary edge cases to maintain Scientific Pitch Notation.
+        const offset: i8 = switch (self.name.acc) {
+            .flat, .double_flat => if (self.name.let == .c) 1 else 0,
+            .natural => 0,
+            .sharp, .double_sharp => if (self.name.let == .b) -1 else 0,
+        };
+
+        return oct + offset;
     }
 
     pub fn pitchClass(self: Note) u4 {
         return @intCast(@mod(self.midi, c.semis_per_oct));
+    }
+
+    pub fn spellWithSharps(midi: u7) Spelling {
+        const pc = @mod(midi, c.semis_per_oct);
+        return switch (pc) {
+            0 => .{ .let = .c, .acc = .natural },
+            1 => .{ .let = .c, .acc = .sharp },
+            2 => .{ .let = .d, .acc = .natural },
+            3 => .{ .let = .d, .acc = .sharp },
+            4 => .{ .let = .e, .acc = .natural },
+            5 => .{ .let = .f, .acc = .natural },
+            6 => .{ .let = .f, .acc = .sharp },
+            7 => .{ .let = .g, .acc = .natural },
+            8 => .{ .let = .g, .acc = .sharp },
+            9 => .{ .let = .a, .acc = .natural },
+            10 => .{ .let = .a, .acc = .sharp },
+            11 => .{ .let = .b, .acc = .natural },
+            else => unreachable,
+        };
+    }
+
+    pub fn spellWithFlats(midi: u7) Spelling {
+        const pc = @mod(midi, c.semis_per_oct);
+        return switch (pc) {
+            0 => .{ .let = .c, .acc = .natural },
+            1 => .{ .let = .d, .acc = .flat },
+            2 => .{ .let = .d, .acc = .natural },
+            3 => .{ .let = .e, .acc = .flat },
+            4 => .{ .let = .e, .acc = .natural },
+            5 => .{ .let = .f, .acc = .natural },
+            6 => .{ .let = .g, .acc = .flat },
+            7 => .{ .let = .g, .acc = .natural },
+            8 => .{ .let = .a, .acc = .flat },
+            9 => .{ .let = .a, .acc = .natural },
+            10 => .{ .let = .b, .acc = .flat },
+            11 => .{ .let = .b, .acc = .natural },
+            else => unreachable,
+        };
     }
 
     pub fn format(
@@ -82,19 +106,16 @@ pub const Note = struct {
     ) !void {
         _ = fmt;
         _ = options;
-        const let = self.letter();
-        const acc = self.accidental();
-        const oct = self.octave();
         try writer.print("{c}{s}{d}", .{
-            std.ascii.toUpper(@tagName(let)[0]),
-            switch (acc) {
+            std.ascii.toUpper(@tagName(self.name.let)[0]),
+            switch (self.name.acc) {
                 .double_flat => "𝄫",
                 .flat => "♭",
                 .natural => "",
                 .sharp => "♯",
                 .double_sharp => "𝄪",
             },
-            oct,
+            self.octave(),
         });
     }
 };
@@ -114,30 +135,28 @@ test "Note initialization" {
 
 test "Note properties" {
     const c4 = try Note.init(.c, .natural, 4);
-    try testing.expectEqual(Note.Letter.c, c4.letter());
-    try testing.expectEqual(Note.Accidental.natural, c4.accidental());
+    try testing.expectEqual(Note.Letter.c, c4.name.let);
+    try testing.expectEqual(Note.Accidental.natural, c4.name.acc);
     try testing.expectEqual(4, c4.octave());
     try testing.expectEqual(0, c4.pitchClass());
 
     const cs4 = try Note.init(.c, .sharp, 4);
-    try testing.expectEqual(Note.Letter.c, cs4.letter());
-    try testing.expectEqual(Note.Accidental.sharp, cs4.accidental());
+    try testing.expectEqual(Note.Letter.c, cs4.name.let);
+    try testing.expectEqual(Note.Accidental.sharp, cs4.name.acc);
     try testing.expectEqual(4, cs4.octave());
     try testing.expectEqual(1, cs4.pitchClass());
 
-    // There's currently no naming persistence.
     const df4 = try Note.init(.d, .flat, 4);
-    try testing.expectEqual(Note.Letter.c, df4.letter());
-    try testing.expectEqual(Note.Accidental.sharp, df4.accidental());
+    try testing.expectEqual(Note.Letter.d, df4.name.let);
+    try testing.expectEqual(Note.Accidental.flat, df4.name.acc);
     try testing.expectEqual(4, df4.octave());
     try testing.expectEqual(1, df4.pitchClass());
 }
 
 test "Note formatting" {
-    // There's currently no naming persistence.
-    try testing.expectFmt("A♯3", "{}", .{try Note.init(.c, .double_flat, 4)});
-    try testing.expectFmt("B3", "{}", .{try Note.init(.c, .flat, 4)});
+    try testing.expectFmt("C𝄫4", "{}", .{try Note.init(.c, .double_flat, 4)});
+    try testing.expectFmt("C♭4", "{}", .{try Note.init(.c, .flat, 4)});
     try testing.expectFmt("C4", "{}", .{try Note.init(.c, .natural, 4)});
     try testing.expectFmt("C♯4", "{}", .{try Note.init(.c, .sharp, 4)});
-    try testing.expectFmt("D4", "{}", .{try Note.init(.c, .double_sharp, 4)});
+    try testing.expectFmt("C𝄪4", "{}", .{try Note.init(.c, .double_sharp, 4)});
 }
